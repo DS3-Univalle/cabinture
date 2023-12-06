c2a7ad649fe60e74fe1691c3e02505a27c840573
Brayan J. Gomez M <94873455+BrianGomezM@users.noreply.github.com>
2023-12-05 23:32:12 -0500

    DS-25-VerPropiedades

--- src/layouts/cabins/addCabin/index.js
+++ src/layouts/cabins/addCabin/index.js
@@ -39,43 +39,89 @@ function AddCabinComponent() {
     }));
   };
 
+  const showAlert = (icon, title) => {
+    Swal.fire({
+      icon,
+      title,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
 
-  const handleClick = async (e) => {
+  const handleCreate = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:8081/create-cabin", cabin, {
+      const response = await axios.post("http://localhost:8081/create-cabin", cabin, {
         headers: {
           'Content-Type': 'application/json',
         },
       });
-      console.log(response.data)
       setCabin(response.data);
-      console.log("done");
-      // Muestra el mensaje de éxito con Sweet Alert
-      Swal.fire({
-        icon: 'success',
-        title: 'Registro guardado con éxito',
-        showConfirmButton: false,
-        timer: 1500,
-      });
-      setAgregarBTN(false);
-      setEliminarBTN(true);
-      setActualizarBTN(true);
+      showAlert('success', 'Registro guardado con éxito');
+      setButtonsState(false, true, true);
     } catch (err) {
       console.log(err);
-      // Muestra un mensaje de error con Sweet Alert
-      Swal.fire({
-        icon: 'error',
-        title: 'Error al guardar el registro',
-      });
+      showAlert('error', 'Error al guardar el registro');
     }
   };
 
+  const handleDelete = async () => {
+    try {
+      const cabinIDToDelete = cabin.id_cabin;
+      if (!cabinIDToDelete) {
+        console.error("ID de cabaña no especificado");
+        return;
+      }
+      await axios.delete(`http://localhost:8081/cabin?cabinID=${cabinIDToDelete}`);
+      showAlert('success', 'Registro eliminado con éxito');
+      resetCabinState();
+      setButtonsState(true, false, false);
+    } catch (err) {
+      console.log(err);
+      showAlert('error', 'Error al eliminar el registro');
+    }
+  };
+
+  const handleUpdate = async () => {
+    try {
+      const response = await axios.patch(`http://localhost:8081/cabin?cabinID=${cabin.id_cabin}`, cabin, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      setCabin(response.data);
+      showAlert('success', 'Registro actualizado con éxito');
+      // Puedes limpiar el formulario o realizar otras acciones después de la actualización
+    } catch (err) {
+      console.log(err);
+      showAlert('error', 'Error al actualizar el registro');
+    }
+  };
+
+  const resetCabinState = () => {
+    setCabin({
+      id_cabin: "",
+      name: "",
+      description: "",
+      location: "",
+      price: null,
+      rooms: null,
+      bathrooms: null,
+      legal_information: "",
+      number_people: null,
+      id_state: 3
+    });
+  };
+
+  const setButtonsState = (add, del, update) => {
+    setAgregarBTN(add);
+    setEliminarBTN(del);
+    setActualizarBTN(update);
+  };
 
   const [agregarBTN, setAgregarBTN] = useState(true);
   const [eliminarBTN, setEliminarBTN] = useState(false);
   const [ActualizarBTN, setActualizarBTN] = useState(false);
-
 
   const [address, setAddress] = useState("");
   const handleAddressChange = (newAddress) => {
@@ -102,7 +148,7 @@ function AddCabinComponent() {
                           Cod Cabaña
                         </SoftTypography>
                       </SoftBox>
-                      <SoftInput type="text" placeholder="Cod Cabaña" name="id_cabin" value={cabin.id_cabin || ''} onChange={handleChange}/>
+                      <SoftInput type="text" placeholder="Cod Cabaña" name="id_cabin" value={cabin.id_cabin || ''} onChange={handleChange} />
                     </SoftBox>
                   </SoftBox>
                   <SoftBox component="ul" display="flex" flexDirection="column" p={0} m={0} sx={{ listStyle: "none" }}>
@@ -122,7 +168,7 @@ function AddCabinComponent() {
                           Precio
                         </SoftTypography>
                       </SoftBox>
-                      <SoftInput type="number" placeholder="$100.000,00" name="price" value={cabin.price || ''}onChange={handleChange} />
+                      <SoftInput type="number" placeholder="$100.000,00" name="price" value={cabin.price || ''} onChange={handleChange} />
                     </SoftBox>
                   </SoftBox>
                   <SoftBox component="ul" display="flex" flexDirection="column" p={0} m={0} sx={{ listStyle: "none" }} >
@@ -211,27 +257,26 @@ function AddCabinComponent() {
                 <SoftBox pt={3} pb={2} px={2}>
                   <Grid container spacing={3}>
                     {agregarBTN && (
-                    <Grid item xs={4}>
-                    
-                      <SoftButton className="softButtonCustomColor" style={{ backgroundColor: "#71C455", color: "white", width: "100%" }} onClick={handleClick} >
-                        &nbsp;Agregar
-                      </SoftButton>
-                    </Grid>)}
+                      <Grid item xs={4}>
+                        <SoftButton className="softButtonCustomColor" style={{ backgroundColor: "#71C455", color: "white", width: "100%" }} onClick={handleCreate} >
+                          &nbsp;Agregar
+                        </SoftButton>
+                      </Grid>)}
                     {ActualizarBTN && (
-                    <Grid item xs={4}>
+                      <Grid item xs={4}>
                         <SoftButton
                           className="softButtonCustomColor"
-                          style={{ backgroundColor: "#FFCC41", color: "white", width: "100%" }}
+                          style={{ backgroundColor: "#FFCC41", color: "white", width: "100%" }} onClick={handleUpdate}
                         >
                           &nbsp;Actualizar
                         </SoftButton>
-                    </Grid>)}
+                      </Grid>)}
                     {eliminarBTN && (
-                    <Grid item xs={4}>
-                        <SoftButton className="softButtonCustomColor" style={{ backgroundColor: "#C45555", color: "white", width: "100%" }} >
+                      <Grid item xs={4}>
+                        <SoftButton className="softButtonCustomColor" style={{ backgroundColor: "#C45555", color: "white", width: "100%" }} onClick={handleDelete}>
                           &nbsp;Eliminar
                         </SoftButton>
-                    </Grid> )}
+                      </Grid>)}
                   </Grid>
                 </SoftBox>
               </Card>
